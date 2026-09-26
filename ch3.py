c6966dcdac10da3d6862ad7e9733824e996c01a2
Nikita Solanki <nikitasolanki0457@gmail.com>
2026-09-26 17:48:22 +0530

    chapter3

---
+++ ch3.py
@@ -0,0 +1,32 @@
+# Escape Sequence Charater  
+# \n used for giving new line
+a = "hey i am nikita \n and i am a student"
+print(a)
+# \t used for tab giving space
+a = "hey i am nikita \t and i am a student"
+print(a)
+# \" \" use for giving commas to a single world
+a = "hey i am nikita \"nik\" and i am a student"
+print(a)
+
+# ALL OPERATIONS OF STRINGS
+
+# Create a simple string
+s = "hello i am nikki"
+# access charater
+print(f"Access the characters in string : \n {s[1]}")
+# Negative Indexing
+print(f"Negative Indexing : \n {s[-1]} ")
+# Slicing
+print(f"Slicing : \n {s[1:2]} ")
+# Conantenation mix two string
+print(f"Conatenation : \n {"nikki" + "solanki"}")
+# Repetations
+print(f"Repetation : \n {"hello" * 3}")
+# Length of the string
+print(f"Length of string : \n {len(s)}")
+# Membersship
+print(f"Membership : \n {"H" in s}")
+# Comparison of string
+print(f"Comparison of string : \n {"nik" == "nik"}")
+print(s)
