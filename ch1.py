b4f2c4c2a1ea77c49ef7c16c0b480fd8443dd4d5
Nikita Solanki <nikitasolanki0457@gmail.com>
2026-09-26 17:11:12 +0530

    chapter1

--- ch1.py
+++ ch1.py
@@ -7,18 +7,13 @@ Like a diamond in the sky."""
 
 import pyttsx3
 engine = pyttsx3.init()
-
-# For Mac, If you face error related to "pyobjc" when running the `init()` method :
-# Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
-
 engine.say("hey i am nikki")
 engine.runAndWait()
 
-import os
 
+import os
 directory_path = "Chapter4"
 contents = os.listdir(directory_path)
-
 print(contents)
 # for item in contents:
 #     print(item)
---
+++ ch2.py
@@ -0,0 +1,43 @@
+dict = {
+    "Name" : "Nikita",
+    "Age" : 20,
+    "Course" : "B.E"
+}
+print("Name :", dict["Name"])
+print("Age :", dict["Age"])
+print("Course :", dict["Course"])
+
+
+# a = "10"
+# t = type(a)
+# print(t)
+# b = 22
+# print (a+b)
+a = int(input("Enter the 1st number:"))
+b = int(input("Enter the 2nd number:"))
+sum = a + b
+print(sum)
+
+
+a = int(input("Enter the 1st number:"))
+b = int(input("Enter the 2nd number:"))
+Remainder = a / b
+print(Remainder)
+
+
+a = int(input("Enter the 1st number:"))
+print(type(a))
+
+
+a = int(input("Enter the 1st number:"))
+b = int(input("Enter the 2nd number:"))
+print(a>b)
+
+
+a = int(input("Enter the 1st number:"))
+b = int(input("Enter the 2nd number:"))
+print(f"Average of {a} and {b} is {(a+b)/2}")
+
+
+a = int(input("Enter the 1st number:"))
+print(f"An Square of the number is {a*a}")
